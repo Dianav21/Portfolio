343ca335f9bb7e2e45142c68376781eef073ec86
Dianav21 <dianavargas214@gmail.com>
2023-07-10 04:00:51 -0400

    edited file

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from "./components/NavBar";
+import { Navbar } from "./components/Navbar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
@@ -11,7 +11,7 @@ function App() {
 
   return (
       <div className="App">
-      <NavBar />
+      <Navbar />
       <Banner />
       <Skills />
       <Projects />
